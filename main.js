5218108cb186dc4a5de2c711ebfbf42c30f1217c
Aeron01 <one.soulreaper@gmail.com>
2021-04-05 18:39:44 +0200

    not perfect but is working

--- main.js
+++ main.js
@@ -2,12 +2,27 @@ const fs = require('fs')
 const readlineSync = require('readline-sync')
 const chalk = require('chalk')
 const { randomInt } = require('crypto')
+const { hangmanPic } = require('./affichage')
+const { clear } = require('console')
 
+// verification de l'existance des fichier dict.txt et score.json
+// et creation de fichier backup de dict.txt et score.json, si ces dernier n'existe pas
 if (!fs.existsSync('./dict.txt' && './score.json')) {
   console.log(chalk.red(`Error: dict.txt or score.json does not exist`))
   process.exit(1)
-}
+} /*else if (fs.existsSync('./dict.bak' && './score.bak')) {
 
+} else {
+  switch ('./dict.bak' || './score.bak') {
+    case './dict.bak':
+      let filecont = fs.readFileSync('./dict.txt', 'utf-8')
+      fs.writeFileSync('./dict.bak', `${filecont}`)
+    case './score.bak':
+      let filecont2 = fs.readFileSync('./score.json', 'utf-8')
+      fs.writeFileSync('./score.bak', `${filecont2}`)
+      break
+  }
+}*/
 
 
 // initialisation du score, du nombre de party et du compteur d'essai global
@@ -23,9 +38,9 @@ const starGame = () => {
   let wordsCrtNb = wordsRan.length
 
 
-  // nb d'essai du joueur
+  // nb d'essai du joueur et du status du pendu
   let plyrTry = 9
-
+  let hang = 0
   // initialisation du compteur de mot trouver
   let count = 0
 
@@ -43,15 +58,22 @@ const starGame = () => {
 
   // boucle du jeu
   while (turnoff = true) {
-    console.log(chalk.yellowBright(`Partie ${partyLeft}/3`))
-    const plyerEnt = readlineSync.question(chalk.blue(`\nQuelle est le mot secret ?\n${emptTab}\nIl te reste ${plyrTry} essais.    Score: ${score} Points.\nChoisi une lettre :`)).toUpperCase()
+    console.log(chalk.yellowBright(`Partie ${partyLeft}/3\n\n`))
+    console.log(chalk.red(hangmanPic[hang]))
+    console.log(chalk.whiteBright(`\n\nS'aura tu deviner le mot masquer ?\n${emptTab}\nIl te reste${chalk.yellow(` ${plyrTry} `)}essais.    Score: ${chalk.magentaBright(`${score}`)} Points.\n`))
+    const plyerEnt = readlineSync.keyIn(chalk.blue('Choisi une lettre (touche 0 pour quitter le jeu):', { limit: ['a', 'z', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'q', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'w', 'x', 'c', 'v', 'b', 'n', '0'] })).toUpperCase()
 
+    //verification si la touche '0' est appuyer pour quiter le jeu
+    if (plyerEnt === '0') {
+      console.log(chalk.cyanBright.bold('GAME OVER'))
+      process.exit(1)
+    }
 
     //verification de lettre entrer par le joueur par rapport à ceux du mot secret
     //et remplacement des '_' par les lettre trouver au index coresspondant à ces derniers
     // incrementation du compteur et du score si la lettre est correcte, sinon decompte d'un essai
     if (tabCaraWord.includes(plyerEnt)) {
-      console.log(chalk.greenBright(`la lettre ${plyerEnt} est bien dans le mot`))
+      console.log(chalk.greenBright(`La lettre ${plyerEnt} est bien dans le mot masquer.`))
       for (let i = 0; i < tabCaraWord.length; i++) {
         if (plyerEnt === tabCaraWord[i]) {
           if (emptTab[i] === tabCaraWord[i]) {
@@ -64,8 +86,9 @@ const starGame = () => {
         }
       }
     } else {
-      console.log(chalk.redBright(`la lettre ${plyerEnt} n'est pas dans le mot`))
+      console.log(chalk.red(`La lettre ${plyerEnt} n'est pas dans le mot masquer.`))
       plyrTry--
+      hang++
     }
     //console.log(`tableu vide du mot secret: ${emptTab}`)
 
@@ -79,8 +102,12 @@ const starGame = () => {
         const enterNam = readlineSync.question(chalk.cyan(`Entrer votre nom`))
         }*/if (readlineSync.keyInYN(chalk.blue('Recommancer ?'))) {
           score = 0
+          let cont1 = fs.readFileSync('./score.json', 'utf-8')
+          fs.writeFileSync('./score.bak', `${cont1}`)
           starGame()
         } else {
+          let cont2 = fs.readFileSync('./score.json', 'utf-8')
+          fs.writeFileSync('./score.bak', `${cont2}`)
           console.log(chalk.cyanBright.bold('GAME OVER'))
           process.exit(1)
         }
@@ -90,11 +117,11 @@ const starGame = () => {
     // si c'est le cas, calcul du score final et affichage du mot, du nombre d'essai utiliser et du score du joueur
     //proposition de recommencer la partie
     if (wordsCrtNb === count) {
-      //plyrTry = plyrTry + count
       score = score + 40 + (plyrTry * 5)
       console.log(chalk.greenBright(`Bravo, vous avez trouvez. le mot etait bien ${wordsRan}.\nVous avez trouver en utilisant ${Math.abs(plyrTry - 9)} essais.\nVotre score est de ${score} Points.`)), endGame()
     } else if (plyrTry === 0) {
-      console.log(chalk.greenBright(`Pas de chance. le mot etait ${wordsRan}.\nVotre score est de ${score} points.`)), endGame()
+      console.log(chalk.red(`\n\n${hangmanPic[9]}\n`))
+      console.log(chalk.redBright(`Pas de chance. le mot etait ${wordsRan}.\nVotre score est de ${score} points.`)), endGame()
     }
   }
 
